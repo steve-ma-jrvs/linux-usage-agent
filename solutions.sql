1e51046d85020600c779b4a618b021e9ee5abd82
steve-ma-jrvs <steve.ma.jrvs.ca@gmail.com>
2019-06-24 15:25:18 -0400

    update solution.sql

--- solutions.sql
+++ solutions.sql
@@ -5,7 +5,19 @@ ORDER BY cpu_number, total_mem DESC;
 
 -- Question 2
 -- User Round 5 on the percentage to show the differences
-SELECT id as host_id, hostname, total_mem, ROUND(((total_mem - avg(memory_free) over w) / total_mem)*100, 5) as used_memory_percentage
+-- Using function given in https://stackoverflow.com/questions/6195439/postgres-how-do-you-round-a-timestamp-up-or-down-to-the-nearest-minute
+CREATE FUNCTION round_minutes(TIMESTAMP WITHOUT TIME ZONE, integer) 
+RETURNS TIMESTAMP WITHOUT TIME ZONE AS $$ 
+  SELECT 
+     date_trunc('hour', $1) 
+     +  cast(($2::varchar||' min') as interval) 
+     * round( 
+     (date_part('minute',$1)::float + date_part('second',$1)/ 60.)::float 
+     / $2::float
+      )
+$$ LANGUAGE SQL IMMUTABLE;
+
+SELECT id as host_id, hostname, total_mem, ROUND(((total_mem - avg(memory_free) over w * 1000 ) / total_mem)*100, 5) as used_memory_percentage
 FROM host_info
 INNER JOIN host_usage on host_info.id = host_usage.host_id
 window w as (partition by round_minutes(host_usage."timestamp", 5));

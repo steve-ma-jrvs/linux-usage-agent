8d3d0ad744827a57fb3ba6de1f668c5842b15c14
steve-ma-jrvs <steve.ma.jrvs.ca@gmail.com>
2019-06-24 13:06:38 -0400

    update

---
+++ README.md
@@ -0,0 +1,37 @@
+## Introduction
+
+Cluster Monitor Agent is an internal tool that monitors the cluster resources such as CPU, memory and disk available of a Linux cluster. It helps the infrastructure team to record and analyze the CPU/Memory 
+
+## Architecture and Design
+
+* The cluster diagram with three linux hosts, a DB and agents
+
+ ![project](/Users/stevenma/Desktop/Jarvis/linux-usage-agent/project.png)
+
+* Create a database named host_agent in Postgres and save data into two tables:
+
+  1. host_info
+
+     table columns: id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, timestamp
+
+  2. host_usage
+
+     table columns: timestamp, host_id, memory_free, cpu_idel, cpu_kernel, disk_io, disk_available
+
+* Create two scripts to insert data into the created table
+  1. `host_info.sh` collects the host hardware info and insert it into the database.
+  2. `host_usage.sh` collects the current host usage (CPU and memory)
+
+## Usage
+
+* Use `init.sql` init database and tables. Run only once.
+* `host_info.sh` run only once to insert the the hostinfo into the host_info table.
+* `host_usage` be triggered by the `crontab` job every minute and insert data into host_usage table.
+* `crontab` setup with every minute to run host_usage and save result into host_usage.log
+
+## Improvements
+
+1. Handle hardware updates
+2. Record the unusual CPU/Memory usage data
+3. Create a backup database on other server as well
+
--- scripts/host_usage.sh
+++ scripts/host_usage.sh
@@ -7,31 +7,52 @@ db_name=$3
 user_name=$4
 password=$5
 
+#Setup functions
+get_timestamp(){
+    timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "timestamp=$timestamp"
+}
+
+get_host_id(){
+    host_id=$(cat ~/host_id | xargs )
+    echo "host_id=$host_id"
+}
+
+get_memory_free(){
+    #in MB
+    memory_free=$(vmstat --unit M | tail -1 | awk '{print $4}' | xargs)
+    echo "memory_free=$memory_free"
+}
+
+get_cpu_idel(){
+    #in percentage
+    cpu_idel=$(vmstat --unit M | tail -1 | awk '{print $(NF-2)}' | xargs)
+    echo "cpu_idel=$cpu_idel"
+}
+
+get_cpu_kernel(){
+    cpu_kernel=$(vmstat --unit M | tail -1 | awk '{print $(NF-3)}' | xargs)
+    echo "cpu_kernel=$cpu_kernel"
+}
+
+get_disk_io(){
+    disk_io=$(vmstat -d | tail -1 | awk '{print $(NF-1)}' | xargs)
+    echo "disk_io=$disk_io"
+}
+
+get_disk_avail(){
+    disk_available=$(df -BM / | tail -1 | awk '{print $4}' | sed 's\M\\' | xargs)
+    echo "disk_available=$disk_available"
+}
 
 #Step 1: parse data and setup variables
-timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-echo "timestamp=$timestamp"
-
-host_id=$(cat ~/host_id | xargs )
-echo "host_id=$host_id"
-
-#in MB
-memory_free=$(vmstat --unit M | tail -1 | awk '{print $4}' | xargs)
-echo "memory_free=$memory_free"
-
-#in percentage
-cpu_idel=$(vmstat --unit M | tail -1 | awk '{print $(NF-2)}' | xargs)
-echo "cpu_idel=$cpu_idel"
-
-cpu_kernel=$(vmstat --unit M | tail -1 | awk '{print $(NF-3)}' | xargs)
-echo "cpu_kernel=$cpu_kernel"
-
-disk_io=$(vmstat -d | tail -1 | awk '{print $(NF-1)}' | xargs)
-echo "disk_io=$disk_io"
-
-disk_available=$(df -BM / | tail -1 | awk '{print $4}' | sed 's\M\\' | xargs)
-echo "disk_available=$disk_available"
-
+get_timestamp
+get_host_id
+get_memory_free
+get_cpu_idel
+get_cpu_kernel
+get_disk_io
+get_disk_avail
 
 #Step 2: construct INSERT Statement
 insert_statement=$(cat <<-END

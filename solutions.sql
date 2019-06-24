ad99926445f4f0c88cecf7bbd8b3fa543039a5ac
steve-ma-jrvs <steve.ma.jrvs.ca@gmail.com>
2019-06-24 15:30:27 -0400

    update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -17,7 +17,7 @@ RETURNS TIMESTAMP WITHOUT TIME ZONE AS $$
       )
 $$ LANGUAGE SQL IMMUTABLE;
 
-SELECT id as host_id, hostname, total_mem, ROUND(((total_mem - avg(memory_free) over w * 1000 ) / total_mem)*100, 5) as used_memory_percentage
+SELECT id as host_id, hostname, total_mem, ROUND(((total_mem - (avg(memory_free) over w) * 1000 ) / total_mem)*100, 5) as used_memory_percentage
 FROM host_info
 INNER JOIN host_usage on host_info.id = host_usage.host_id
 window w as (partition by round_minutes(host_usage."timestamp", 5));
